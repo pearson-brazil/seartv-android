5841ccd1274d60c2452a0ae51307f8428324aafe
rafael aramizu <rafaelaramizu@tokenlab.com.br>
2016-10-19 20:59:42 -0200

    Criação do menu overflow com a opção 'sobre'

--- SearTV/app/src/main/java/br/com/seartv/ui/HomeActivity.java
+++ SearTV/app/src/main/java/br/com/seartv/ui/HomeActivity.java
@@ -1,15 +1,39 @@
 package br.com.seartv.ui;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 
 import br.com.seartv.R;
 
-public class HomeActivity extends AppCompatActivity {
+public class HomeActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        setHomeButton();
+        setToolbarTitle(R.string.movies);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        super.onCreateOptionsMenu(menu);
+        getMenuInflater().inflate(R.menu.home_menu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch(item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                break;
+            case R.id.about:
+                break;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
