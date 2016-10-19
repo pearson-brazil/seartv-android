b800087ebc89a10656a53eedf10c0583a3969f60
rafael aramizu <rafaelaramizu@tokenlab.com.br>
2016-10-19 19:58:30 -0200

    Criação das classes do Model

--- SearTV/app/src/main/java/br/com/seartv/ui/HomeActivity.java
+++ SearTV/app/src/main/java/br/com/seartv/ui/HomeActivity.java
@@ -1,7 +1,9 @@
-package br.com.seartv;
+package br.com.seartv.ui;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import br.com.seartv.R;
 
 public class HomeActivity extends AppCompatActivity {
 
