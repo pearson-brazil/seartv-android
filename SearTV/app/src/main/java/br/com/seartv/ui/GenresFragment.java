99b89df30560e053a23ad958b65733d82096742d
rafael aramizu <rafaelaramizu@tokenlab.com.br>
2016-10-23 14:32:34 -0200

    Listview na tela de Gêneros carregada com array de string mockado

--- SearTV/app/src/main/java/br/com/seartv/ui/GenresFragment.java
+++ SearTV/app/src/main/java/br/com/seartv/ui/GenresFragment.java
@@ -1,15 +1,25 @@
 package br.com.seartv.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 import br.com.seartv.R;
+import butterknife.Bind;
 import butterknife.ButterKnife;
 
 public class GenresFragment extends Fragment {
+
+    @Bind(R.id.lst_genres)
+    ListView lstGenres;
+
+    String[] genres;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -28,5 +38,19 @@ public class GenresFragment extends Fragment {
     }
 
     private void fillInformation() {
+
+        genres = getResources().getStringArray(R.array.genres_category);
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, genres);
+        lstGenres.setAdapter(adapter);
+
+        lstGenres.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+
+                Intent intent = new Intent(GenresFragment.this.getActivity(), MovieDetailsActivity.class);
+                startActivity(intent);
+            }
+        });
     }
 }
