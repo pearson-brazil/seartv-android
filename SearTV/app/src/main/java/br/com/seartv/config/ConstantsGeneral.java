04cb33d2e756190c7de4464d2582bb6896a11619
rafael aramizu <rafaelaramizu@tokenlab.com.br>
2016-10-25 21:04:29 -0200

    Constantes utilizadas por todas as chamadas

---
+++ SearTV/app/src/main/java/br/com/seartv/config/ConstantsGeneral.java
@@ -0,0 +1,9 @@
+package br.com.seartv.config;
+
+public class ConstantsGeneral {
+
+    public static final String TIME_ZONE_DATE_FORMAT = "yyyy-MM-dd'T'HH:mm:ssZ";
+    public static final String ACCEPT = "Accept";
+    public static final String JSON = "application/json";
+    public static final String MESSAGE = "message";
+}
