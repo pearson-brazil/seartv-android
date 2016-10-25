cfa4e735380df718d7e5cefd7d2b693c0e0cc31c
rafael aramizu <rafaelaramizu@tokenlab.com.br>
2016-10-25 21:04:04 -0200

    Métodos úteis para a camada de serviço. Métodos utilizados para montar header de requisição, parser de Json, pilha de requisição da Volley

--- SearTV/app/src/main/java/br/com/seartv/utils/Utils.java
+++ SearTV/app/src/main/java/br/com/seartv/utils/Utils.java
@@ -2,11 +2,16 @@ package br.com.seartv.utils;
 
 import android.content.Context;
 
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
 import org.json.JSONException;
 import org.json.JSONObject;
 
 import java.util.HashMap;
 import java.util.Map;
+
+import br.com.seartv.config.ConstantsGeneral;
 
 public class Utils {
 
@@ -14,5 +19,34 @@ public class Utils {
         String url = "https://www.google.com.br/#q=";
         url = url + name.toLowerCase().replace(" ", "+");
         return url;
+    }
+
+    public static Object parseJsonToObject(JSONObject jsonObject, Class definedClass) {
+        GsonBuilder gsonBuilder = new GsonBuilder();
+        gsonBuilder.setDateFormat(ConstantsGeneral.TIME_ZONE_DATE_FORMAT);
+        Gson gson = gsonBuilder.create();
+
+        return gson.fromJson(jsonObject.toString(), definedClass);
+    }
+
+    public static Map<String, String> generalHeader(Context ctx) {
+        Map<String, String> headers = new HashMap<>();
+        headers.put(ConstantsGeneral.ACCEPT, ConstantsGeneral.JSON);
+
+        return headers;
+    }
+
+    public static String trimMessage(String json, String key) {
+        String trimmedString = null;
+
+        try {
+            JSONObject obj = new JSONObject(json);
+            trimmedString = obj.getString(key);
+        } catch (JSONException e) {
+            e.printStackTrace();
+            return null;
+        }
+
+        return trimmedString;
     }
 }
